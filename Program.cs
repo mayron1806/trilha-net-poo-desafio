04f5654b0f1caa7317b1c16e59233171be878af8
Mayron <mayronfernandes01@gmail.com>
2022-10-14 17:22:46 -0300

    resultado final

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone(numero: "999999999", "8s", "00000000000001", 8);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+
+Nokia nokia = new Nokia(numero: "888888888", "1", "00000000011", 1);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Calculadora");
